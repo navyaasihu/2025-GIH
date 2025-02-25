bb62dd8f94744495b94a4d42fe0c1b4e19954292
ns_110211 <133208903+navyaasihu@users.noreply.github.com>
2025-02-25 17:01:36 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,9 +1,20 @@
 import pandas as pd
 from joblib import load
 
-loaded_model = load("model.joblib")
+loaded_model=load("model.joblib")
 
-new_signal = pd.DataFrame([[3, 2, 1]], columns=["Gates", "Fan-In", "Fan-Out"])
+test_circuits={
+  "3-to-8 Decoder" : [10,3,1],
+  "2-bit Comparator" : [8,2,2],
+  "4-bit Priority Encoder" : [12,3,1],
+  "2-bit ALU" : [16,3,2],
+  "2-bit Up/Down Counter" : [10,2,2]
+}
 
-predicted_depth = loaded_model.predict(new_signal)
-print("Predicted Depth:", predicted_depth[0])
+test_df= pd.DataFrame.from_dict(test_circuits, orient="index", columns=["Gates", "Fan-In", "Fan-Out"])
+
+predicted_depths=loaded_model.predict(test_df)
+
+print("\nPredicted Combinational Depth for NEew RTL Designs:")
+for circuit, depth in zip(test_circuits.keys(), predicted_depths):
+  print(f"{circuit}: {depth:.2f})
